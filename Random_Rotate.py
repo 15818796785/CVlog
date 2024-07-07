7b413adfdb8ce22338e7ff3a2030396eb3e7a0aa
ccymm <119406407+ccymm@users.noreply.github.com>
2024-07-08 00:54:03 +0800

    add Random_Rotate.py RandomRotateMaskset and RandomRotateset(read the description plz!!)
    由于是random rotate，我直接使用detector对戴口罩和没带口罩的旋转后图片进行裁剪，加入了异常处理后，发现没带口罩的图片基本可以识别到，带了口罩的只有一部分（大概一半）成功识别到了面部并实现了裁剪

--- Random_Rotate.py
+++ Random_Rotate.py
@@ -1,11 +1,13 @@
 import preprocessing_utils
 import dlib
+import detector3
+import detector2
 import cv2
 import os
 import tqdm
 
-dataset_path = "GeorgiaTechFaces/Crop_1"
-maskeddataset_path = "GeorgiaTechFaces/Maskedcrop_1"
+dataset_path = "GeorgiaTechFaces/Dataset_1"
+maskeddataset_path = "GeorgiaTechFaces/Maskedset_1"
 predictor_path = 'shape_predictor_68_face_landmarks.dat/shape_predictor_68_face_landmarks.dat'
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(predictor_path)
@@ -56,16 +58,61 @@ for x_list in tqdm.tqdm(X_masked, desc='adding masks'):
         temp_X_blured.append(blured_face)
     X_masked_RandomRotate.append(temp_X_blured)
 
+
+X_maskprocessed = []
+X_processed = []
+print(len(X))
+for i in tqdm.tqdm(range(len(X)), desc='preprocessing images '):
+    temp_X_processed = []
+    temp_X_maskprocessed = []
+    x_list = X_RandomRotate[i]
+    x_masklist = X_masked_RandomRotate[i]
+    for j in range(len(x_list)):
+        # write the code to detect face in the image (x) using dlib facedetection library
+        # write the code to crop the image (x) to keep only the face, resize the cropped image to 150x150
+
+        # temp_img, temp_maskimg = detector3.crop_and_resize_face(x_list[j], x_masklist[j], detector, predictor)
+        temp_img = detector2.crop_and_resize_face(x_list[j], detector, predictor)
+        temp_maskimg = detector2.crop_and_resize_face(x_masklist[j],detector,predictor)
+        # write the code to convert the image (x) to grayscale
+        # gray_image = cv2.cvtColor(temp_img, cv2.COLOR_BGR2GRAY)
+        # gray_mask_image = cv2.cvtColor(temp_maskimg, cv2.COLOR_BGR2GRAY)
+        # 计算直方图
+        # hist, bins = np.histogram(gray_image.flatten(), bins=256, range=[0, 256])
+
+        # plt.figure()
+        # plt.title("Grayscale Histogram")
+        # plt.xlabel("Gray level")
+        # plt.ylabel("Frequency")
+        # plt.plot(hist)
+        # plt.xlim([0, 256])
+        # plt.show()
+        # dlib.hit_enter_to_continue()
+
+        # append the converted image into temp_X_processed
+        temp_X_processed.append(temp_img)
+        temp_X_maskprocessed.append(temp_maskimg)
+
+    # append temp_X_processed into  X_processed
+    X_processed.append(temp_X_processed)
+    X_maskprocessed.append(temp_X_maskprocessed)
+
 X_RandomRotate_dataset_path = 'GeorgiaTechFaces/RandomRotateprocessedset_1'
-for i, subject_images in enumerate(X_RandomRotate):
+for i, subject_images in enumerate(X_processed):
     subject_folder = os.path.join(X_RandomRotate_dataset_path, f"s{str(i + 1).zfill(2)}")
     os.makedirs(subject_folder, exist_ok=True)
     for j, img in enumerate(subject_images):
-        cv2.imwrite(os.path.join(subject_folder, f"{str(j + 1).zfill(2)}.jpg"), img)
+        try:
+            cv2.imwrite(os.path.join(subject_folder, f"{str(j + 1).zfill(2)}.jpg"), img)
+        except Exception as e:
+            continue
 
 X_masked_RandomRotate_dataset_path = 'GeorgiaTechFaces/RandomRotateMaskprocessedset_1'
-for i, subject_images in enumerate(X_masked_RandomRotate):
+for i, subject_images in enumerate(X_maskprocessed):
     subject_folder = os.path.join(X_masked_RandomRotate_dataset_path, f"s{str(i + 1).zfill(2)}")
     os.makedirs(subject_folder, exist_ok=True)
     for j, img in enumerate(subject_images):
-        cv2.imwrite(os.path.join(subject_folder, f"{str(j + 1).zfill(2)}.jpg"), img)
+        try:
+            cv2.imwrite(os.path.join(subject_folder, f"{str(j + 1).zfill(2)}.jpg"), img)
+        except Exception as e:
+            continue
