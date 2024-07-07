20d1a356d759a3920f328de2a386670f822521ba
xyyzhhh <xyyzhhhh@sjtu.edu.cn>
2024-07-07 11:49:43 +0800

    Update T4_2.py
    accuracy not 100%

--- T4_2.py
+++ T4_2.py
@@ -33,13 +33,13 @@ for subject_name in tqdm.tqdm(os.listdir(processedset_path), desc='reading proce
         for img_name in os.listdir(subject_images_dir):
             if img_name.endswith('.jpg'):
                 img_path = os.path.join(subject_images_dir, img_name)
-                img = cv2.imread(img_path)
+                img = cv2.imread(img_path, flags=0)
                 # gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
                 temp_x_list.append(img)
         X_processed.append(temp_x_list)
 
 X_masked_features = []
-for x_list in X_masked:
+for x_list in tqdm.tqdm(X_masked, desc="HOG masked images", unit="image"):
     temp_X_features = []
     for x in x_list:
         x_feature = hog(x, orientations=8, pixels_per_cell=(10, 10),
@@ -56,10 +56,10 @@ X_processed_features = []
 #         temp_X_features.append(x_feature)
 #     X_processed_features.append(temp_X_features)
 
-for x_list in tqdm.tqdm(X_processed, desc="Processing images", unit="image"):
+for x_list in tqdm.tqdm(X_processed, desc="HOG processed images", unit="image"):
     temp_X_features = []
     for x in x_list:
-        x_feature = hog(cv2.cvtColor(x, cv2.COLOR_BGR2GRAY), orientations=8, pixels_per_cell=(10, 10),
+        x_feature = hog(x, orientations=8, pixels_per_cell=(10, 10),
                         cells_per_block=(1, 1), visualize=False)
         temp_X_features.append(x_feature)
     X_processed_features.append(temp_X_features)
@@ -77,7 +77,8 @@ X = X_masked_flat + X_processed_flat
 y = y_masked + y_processed
 
 # Split the dataset into training and testing sets (80-20 split)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1475, shuffle=True, random_state=42) #, random_state=42
+print(len(X_train), len(X_test), len(y_train), len(y_test))
 
 # Train the SVM classifier
 svm_classifier = SVC(kernel='linear')
@@ -87,6 +88,8 @@ svm_classifier.fit(X_train, y_train)
 
 print("Testing the classifier...")
 y_pred = svm_classifier.predict(X_test)
+# print(y_test)
+# print(y_pred)
 
 # Evaluate the classifier
 accuracy = accuracy_score(y_test, y_pred)
