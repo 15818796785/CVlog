91144e21be1061b7a3ed9d50eb80a04f7d4ccc26
ccymm <119406407+ccymm@users.noreply.github.com>
2024-07-11 19:59:15 +0800

    Update Mask.py

--- 20_prepocessing/Mask.py
+++ 20_prepocessing/Mask.py
@@ -8,8 +8,8 @@ from skimage.feature import hog
 import cv2
 import tqdm
 
-dataset_path = "../20_GeorgiaTechFaces/img_align_celeba/img_align_celeba"
-predictor_path = '../shape_predictor_68_face_landmarks.dat/shape_predictor_68_face_landmarks.dat'
+dataset_path = "img_align_celeba"
+predictor_path = 'shape_predictor_68_face_landmarks.dat/shape_predictor_68_face_landmarks.dat'
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(predictor_path)
 
@@ -17,14 +17,12 @@ X = []
 count = 0
 for img_name in tqdm.tqdm(os.listdir(dataset_path), desc='reading images'):
     if img_name.endswith('.jpg'):
-        count += 1
-        if count % 10 == 0:  # 每隔十个图片读取一张
-            img_path = os.path.join(dataset_path, img_name)
-            img = cv2.imread(img_path)
-            if img is not None:
-                X.append(img)
-            if len(X) >= 20000:  # 达到20000张图片时结束
-                break
+        img_path = os.path.join(dataset_path, img_name)
+        img = cv2.imread(img_path)
+        if img is not None:
+            X.append(img)
+        if len(X) >= 20000:  # 达到20000张图片时结束
+            break
 # add the temp_x_list to X
 
 def add_mask(image, landmarks):
@@ -40,6 +38,7 @@ def add_mask(image, landmarks):
 X_masked = []
 
 for x in tqdm.tqdm(X, desc='adding masks'):
+    if len(X_masked) > 100: break
     dets = detector(x, 1)
     if len(dets) == 0:
         print('no featrues')
@@ -49,7 +48,11 @@ for x in tqdm.tqdm(X, desc='adding masks'):
         masked_face = add_mask(x, landmarks)
         X_masked.append(masked_face)
 
-Masked_dataset_path = "../20_GeorgiaTechFaces/masked"
+Masked_dataset_path = "20_GeorgiaTechFaces/masked"
+
+# 确保目录存在，如果不存在则创建
+if not os.path.exists(Masked_dataset_path):
+    os.makedirs(Masked_dataset_path)
 # Save the processed images
 for i, img in enumerate(X_masked):
     cv2.imwrite(os.path.join(Masked_dataset_path, f"{str(i + 1).zfill(2)}.jpg"), img)
