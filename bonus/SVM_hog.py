e907c9ad9cd4295619be14df1ee5300a334f10d6
ccymm <119406407+ccymm@users.noreply.github.com>
2024-07-14 16:58:05 +0800

    add divide.py in bonus
    返回的是图片，预处理需要自行完成

--- bonus/SVM_hog.py
+++ bonus/SVM_hog.py
@@ -10,46 +10,65 @@ from sklearn.metrics import accuracy_score, confusion_matrix, classification_rep
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+from divide import read_and_divide_data
 # 数据集路径
 dataset_path = 'GeorgiaTechFaces/Processedset_1'
 masked_path = 'GeorgiaTechFaces/Maskprocessedset_1'
 
-# 读取未戴口罩的数据并提取HOG特征
-X_train = []
-y_train = []
-for subject_name in tqdm.tqdm(os.listdir(dataset_path), desc='reading unmasked images'):
-    if os.path.isdir(os.path.join(dataset_path, subject_name)):
-        subject_images_dir = os.path.join(dataset_path, subject_name)
-        for img_name in os.listdir(subject_images_dir):
-            if img_name.endswith('.jpg'):
-                img_path = os.path.join(subject_images_dir, img_name)
-                img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-                img = cv2.resize(img, (150, 150))
-                hog_features = hog(img, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=False, transform_sqrt = True)
-                X_train.append(hog_features)
-                y_train.append(int(subject_name[1:]))
+# # 读取未戴口罩的数据并提取HOG特征
+# X_train = []
+# y_train = []
+# for subject_name in tqdm.tqdm(os.listdir(dataset_path), desc='reading unmasked images'):
+#     if os.path.isdir(os.path.join(dataset_path, subject_name)):
+#         subject_images_dir = os.path.join(dataset_path, subject_name)
+#         for img_name in os.listdir(subject_images_dir):
+#             if img_name.endswith('.jpg'):
+#                 img_path = os.path.join(subject_images_dir, img_name)
+#                 img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+#                 img = cv2.resize(img, (150, 150))
+#                 hog_features = hog(img, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=False, transform_sqrt = True)
+#                 X_train.append(hog_features)
+#                 y_train.append(int(subject_name[1:]))
 
+# X_train = np.array(X_train)
+# y_train = np.array(y_train)
+
+# # 读取戴口罩的数据并提取HOG特征
+# X_test = []
+# y_test = []
+# for subject_name in tqdm.tqdm(os.listdir(masked_path), desc='reading masked images'):
+#     if os.path.isdir(os.path.join(masked_path, subject_name)):
+#         subject_images_dir = os.path.join(masked_path, subject_name)
+#         for img_name in os.listdir(subject_images_dir):
+#             if img_name.endswith('.jpg'):
+#                 img_path = os.path.join(subject_images_dir, img_name)
+#                 img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+#                 img = cv2.resize(img, (150, 150))
+#                 hog_features = hog(img, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=False, transform_sqrt = True)
+#                 X_test.append(hog_features)
+#                 y_test.append(int(subject_name[1:]))
+
+# X_test = np.array(X_test)
+# y_test = np.array(y_test)
+X_train, X_test, y_train, y_test = read_and_divide_data(dataset_path, masked_path)
+print("length_of_X_train:{}".format(len(X_train)))
+print("length_of_X_test:{}".format(len(X_test)))
+temp_X_train=[]
+temp_X_test=[]
+for img in X_train:
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    hogfeature = hog(img, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=False, transform_sqrt = True)
+    temp_X_train.append(hogfeature)
+
+X_train = temp_X_train
+for img in X_test:
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    hogfeature = hog(img, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=False, transform_sqrt = True)
+    temp_X_test.append(hogfeature)
+
+X_test = temp_X_test
 X_train = np.array(X_train)
-y_train = np.array(y_train)
-
-# 读取戴口罩的数据并提取HOG特征
-X_test = []
-y_test = []
-for subject_name in tqdm.tqdm(os.listdir(masked_path), desc='reading masked images'):
-    if os.path.isdir(os.path.join(masked_path, subject_name)):
-        subject_images_dir = os.path.join(masked_path, subject_name)
-        for img_name in os.listdir(subject_images_dir):
-            if img_name.endswith('.jpg'):
-                img_path = os.path.join(subject_images_dir, img_name)
-                img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-                img = cv2.resize(img, (150, 150))
-                hog_features = hog(img, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=False, transform_sqrt = True)
-                X_test.append(hog_features)
-                y_test.append(int(subject_name[1:]))
-
 X_test = np.array(X_test)
-y_test = np.array(y_test)
-
 # 标准化数据
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
