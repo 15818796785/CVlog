3b6e0f5dd3f6c1930d37bed2e9a9a06196c2de21
ccymm <119406407+ccymm@users.noreply.github.com>
2024-07-15 16:57:37 +0800

    Update SVM_hog.py

--- bonus/SVM_hog.py
+++ bonus/SVM_hog.py
@@ -11,10 +11,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 from divide import read_and_divide_data
+import train_test_split
 # 数据集路径
-dataset_path = 'GeorgiaTechFaces/Processedset_1'
-masked_path = 'GeorgiaTechFaces/Maskprocessedset_1'
-
+# dataset_path = 'GeorgiaTechFaces/ConvertGrayscaleprocessedset_1'
+# masked_path = 'GeorgiaTechFaces/ConvertGrayScaleMaskprocessedset_1'
+dataset_path = '20_GeorgiaTechFaces/dataset/part_1'
+masked_path = '20_GeorgiaTechFaces/masked/part_1'
 # # 读取未戴口罩的数据并提取HOG特征
 # X_train = []
 # y_train = []
@@ -50,33 +52,40 @@ masked_path = 'GeorgiaTechFaces/Maskprocessedset_1'
 
 # X_test = np.array(X_test)
 # y_test = np.array(y_test)
-X_train, X_test, y_train, y_test = read_and_divide_data(dataset_path, masked_path)
+# X_train, X_test, y_train, y_test = read_and_divide_data(dataset_path, masked_path)
+X_train, y_train = train_test_split.train_split(dataset_path)
+X_test, y_test = train_test_split.test_split(masked_path)
 print("length_of_X_train:{}".format(len(X_train)))
 print("length_of_X_test:{}".format(len(X_test)))
-temp_X_train=[]
-temp_X_test=[]
-for img in X_train:
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    hogfeature = hog(img, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=False, transform_sqrt = True)
-    temp_X_train.append(hogfeature)
+# temp_X_train=[]
+# temp_X_test=[]
+# for img in X_train:
+#     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#     hogfeature = hog(img, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=False, transform_sqrt = True)
+#     temp_X_train.append(hogfeature)
 
-X_train = temp_X_train
-for img in X_test:
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    hogfeature = hog(img, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=False, transform_sqrt = True)
-    temp_X_test.append(hogfeature)
+# X_train = temp_X_train
+# for img in X_test:
+#     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#     hogfeature = hog(img, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=False, transform_sqrt = True)
+#     temp_X_test.append(hogfeature)
 
-X_test = temp_X_test
+# X_test = temp_X_test
 X_train = np.array(X_train)
 X_test = np.array(X_test)
 # 标准化数据
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
-
+print("len_X_train[1]:{}".format(len(X_train[1])))
+print("len_X_test[1]:{}".format(len(X_test[1])))
+print("len_X_train:{}".format(len(X_train)))
+print("len_X_test:{}".format(len(X_test)))
+print("len_y_train:{}".format(len(y_train)))
+print("len_y_test:{}".format(len(y_test)))
 svm = SVC(C=0.1, kernel='linear', gamma='auto')
 svm.fit(X_train, y_train)
-
+print("here!")
 # 使用训练好的模型进行预测
 y_pred = svm.predict(X_test)
 
